5c914556a635bb93bf394bfbf89e5563496ee53b
gasimovhikmat <gasimov.hikmat95@gmail.com>
2022-03-14 20:28:18 +0400

    added

---
+++ spring-cloud-conifg-server/spring-cloud-conifg-server/src/test/java/com/microservices/springcloudconifgserver/SpringCloudConifgServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.springcloudconifgserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConifgServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
